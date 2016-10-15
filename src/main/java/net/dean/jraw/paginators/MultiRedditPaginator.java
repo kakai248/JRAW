dfd13b3d91bf82c7726e7c1d0c360d050adcad15
kakai248 <r.kakai248@gmail.com>
2016-10-15 23:12:35 +0100

    Changed MultiRedditPaginator to accept a path instead of a MultiReddit model

--- src/main/java/net/dean/jraw/paginators/MultiRedditPaginator.java
+++ src/main/java/net/dean/jraw/paginators/MultiRedditPaginator.java
@@ -8,7 +8,18 @@ import net.dean.jraw.models.Submission;
  * Iterates through the posts in a multireddit.
  */
 public class MultiRedditPaginator extends Paginator<Submission> {
+
     private MultiReddit multiReddit;
+    private String path;
+
+    /**
+     * Instantiates a new MultiRedditPaginator
+     *
+     * @param creator The RedditClient that will be used to send HTTP requests
+     */
+    public MultiRedditPaginator(RedditClient creator) {
+        super(creator, Submission.class);
+    }
 
     /**
      * Instantiates a new MultiRedditPaginator
@@ -19,12 +30,24 @@ public class MultiRedditPaginator extends Paginator<Submission> {
     public MultiRedditPaginator(RedditClient creator, MultiReddit multi) {
         super(creator, Submission.class);
         this.multiReddit = multi;
+        this.path = multi.getPath();
+    }
+
+    /**
+     * Instantiates a new MultiRedditPaginator
+     *
+     * @param creator The RedditClient that will be used to send HTTP requests
+     * @param path The multireddit path to iterate
+     */
+    public MultiRedditPaginator(RedditClient creator, String path) {
+        super(creator, Submission.class);
+        this.path = path;
     }
 
     @Override
     protected String getBaseUri() {
         String path = sorting == null ? "" : "/" + sorting.name().toLowerCase();
-        return multiReddit.getPath() + path;
+        return this.path + path;
     }
 
     /**
@@ -41,6 +64,16 @@ public class MultiRedditPaginator extends Paginator<Submission> {
      */
     public void setMultiReddit(MultiReddit multiReddit) {
         this.multiReddit = multiReddit;
+        this.path = multiReddit.getPath();
+        invalidate();
+    }
+
+    /**
+     * Sets the MultiReddit path to iterate through
+     * @param path The new MultiReddit path
+     */
+    public void setPath(String path) {
+        this.path = path;
         invalidate();
     }
 }
